b49f1d01e9f19a319917dc74dae69a3081205d5a
goregoreda <rlarudgkswkd@gmail.com>
2022-10-17 17:47:55 +0900

    2022-10-17 입력 폼 유효성검사

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,3 +1,16 @@
+
+//연락처 등록 시 유효성 검사
+$(function(){
+       $("#name").keyup(function (event) {
+            regexp = /[a-z0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:\"'\\]/g;
+            v = $(this).val();
+            if (regexp.test(v)) {
+                alert("한글만 입력가능 합니다.");
+                $(this).val(v.replace(regexp, ''));
+            }
+        });
+});
+
 var main = {
     init : function() {
         var _this = this;
@@ -13,6 +26,8 @@ var main = {
             phone_number : $('#phone_number').val()
         };
 
+        if (!validation_check(data)) { return false;}
+
         $.ajax({
             type:'POST',
             url : '/posts/save',
@@ -27,5 +42,17 @@ var main = {
         });
     }
 };
+
+function validation_check(data){
+    var inputAge = $("#age").val();
+    var setAge = 20;
+
+    if (inputAge > setAge){
+        alert("Sorry, only persons over the age of " + setAge + " may enter this site");
+        return false;
+    }
+    return true;
+}
+
 
 main.init();
