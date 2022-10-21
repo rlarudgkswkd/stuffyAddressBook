02ef538d9ac936115d550c9bba7200a707bf8d2e
goregoreda <rlarudgkswkd@gmail.com>
2022-10-21 16:30:02 +0900

    2022-10-21 작업

--- src/main/java/com/springboot/webapplication/web/controller/MainController.java
+++ src/main/java/com/springboot/webapplication/web/controller/MainController.java
@@ -1,15 +1,11 @@
 package com.springboot.webapplication.web.controller;
 
-import com.springboot.webapplication.connection.jpa.Posts;
-import com.springboot.webapplication.web.dto.PostsListDto;
 import com.springboot.webapplication.web.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Controller
--- src/main/java/com/springboot/webapplication/web/controller/PostsController.java
+++ src/main/java/com/springboot/webapplication/web/controller/PostsController.java
@@ -1,14 +1,10 @@
 package com.springboot.webapplication.web.controller;
 
-import com.springboot.webapplication.connection.jpa.PostsRepository;
 import com.springboot.webapplication.web.dto.PostsSaveDto;
 import com.springboot.webapplication.web.dto.PostsUpdateDto;
 import com.springboot.webapplication.web.service.PostsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
 
 @RequiredArgsConstructor
 @RestController
